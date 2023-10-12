3a4aeb49d6f333c3e682630862bc34a2f801948f
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2023-10-12 07:06:16 -0500

    Detener 36. Implementando las Propiedades para Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected const PRODCUT_AVAILABLE = 'available';
+    protected const PRODCUT_UNAVAILABLE = 'Unavailable';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function isAvailable(): bool
+    {
+        return $this->status == self::PRODCUT_AVAILABLE;
+    }
 }
