d12941fecc5b385af897e510c59d1448c48d64e7
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2023-10-12 07:09:34 -0500

    37. Implementando las Propiedades para Transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'quantity',
+        'product_id',
+        'buyer_id',
+    ];
 }
